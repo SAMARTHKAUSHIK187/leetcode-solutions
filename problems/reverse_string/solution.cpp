41e2990ee7c3d1c59c049be08d18f3545aaf2146
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-01-07 08:24:32 +0000

    Sync LeetCode submission - Reverse String (cpp)

---
+++ problems/reverse_string/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int len = s.size();
+
+        int st = 0;
+        int e = len - 1;
+        while(st <= e){
+            swap(s[st], s[e]);
+            st++;
+            e--;
+        }
+
+
+        // reverse(s.begin(), s.end());
+    }
+};
