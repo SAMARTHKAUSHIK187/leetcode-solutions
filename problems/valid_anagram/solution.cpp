6308a99eb381dbec4fb52d0e890af6002d4eec5a
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-01-16 04:59:27 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- problems/valid_anagram/solution.cpp
+++ problems/valid_anagram/solution.cpp
@@ -5,9 +5,25 @@ public:
             return false;
         }
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        int count1[26] = {0};
+        int count2[26] = {0};
 
-        return s == t;
+        for(int i = 0; i < s.length(); i++){
+            char c1 = s[i];
+            int i1 = c1 - 'a';
+            count1[i1]++;
+
+            char c2 = t[i];
+            int i2 = c2 - 'a';
+            count2[i2]++;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(count1[i] != count2[i]){
+                return false;
+            }
+        }
+
+        return true;
     }
 };
