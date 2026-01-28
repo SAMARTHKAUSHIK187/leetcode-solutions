611fd048a5701359e4b2d26553af7f128d232899
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-01-28 09:08:00 +0000

    Sync LeetCode submission - Count Primes (cpp)

---
+++ problems/count_primes/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> primes(n + 1, true);
+
+        int cnt = 0;
+        primes[0] = primes[1] = false;
+        for(int i = 2; i < n; i++){
+            if(primes[i]){
+                cnt ++;
+
+                for(int j = 2 * i; j < n; j = j + i){
+                    primes[j] = false;
+                }
+                
+            }
+        }
+
+        return cnt;
+        
+    }
+};
