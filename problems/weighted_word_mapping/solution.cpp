97ff7e1be20ee8abace055f2ce9fd37866ded2cf
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-02-14 14:56:43 +0000

    Sync LeetCode submission - Weighted Word Mapping (cpp)

---
+++ problems/weighted_word_mapping/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+
+        string ans = "";
+        
+        for(int i = 0; i < words.size(); i++){
+            string s = words[i];
+            
+            int sum = 0;
+            
+            for(int j = 0;j < s.size(); j++){
+                int n = s[j] - 'a';
+                sum += weights[n];
+            }
+            
+            int n = sum % 26;
+            ans += 'z' - n;
+            
+            
+            
+            
+        }
+
+        return ans;
+    }
+};
