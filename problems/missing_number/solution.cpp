843cfce8a2648921708be8ef21fc4235a3adbaa6
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-03-26 12:40:09 +0000

    Sync LeetCode submission - Missing Number (cpp)

--- problems/missing_number/solution.cpp
+++ problems/missing_number/solution.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int sum = 0;
+        unordered_map<int, int> mpp;
         int n = nums.size();
-        for(int i = 0; i < nums.size(); i++){
-            sum += nums[i];
+        for(int num : nums){
+            mpp[num]++;
+        }
+        for(int i = 0; i <= n; i++){
+            if(mpp[i] == 0){
+                return i;
+            }
         }
 
-        return (n*(n + 1))/2 - sum ;
-        
+        return {};
     }
 };
