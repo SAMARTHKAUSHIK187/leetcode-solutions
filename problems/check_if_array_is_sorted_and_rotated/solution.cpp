095396fa2b2ac87a95e8d7a2b4fbeaeec20f7f81
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-05-23 01:33:21 +0000

    Sync LeetCode submission - Check if Array Is Sorted and Rotated (cpp)

--- problems/check_if_array_is_sorted_and_rotated/solution.cpp
+++ problems/check_if_array_is_sorted_and_rotated/solution.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int count = 0;
-        int n = nums.size();
-        for(int i = 1; i < n; i++){
-            if(nums[i-1] > nums[i]){
-                count ++;
+        int cnt = 0;
+        for(int i = 0; i < nums.size() - 1; i++){
+            if(nums[i] > nums[i + 1]){
+                cnt++;
             }
         }
-
-        if(nums[0] < nums[n-1]){
-            count ++;
+        if(nums[0] < nums[nums.size()-1]){
+            cnt ++;
         }
-        return count <= 1;
-
+        if(cnt <= 1) return true;
+        else return false;
     }
 };
