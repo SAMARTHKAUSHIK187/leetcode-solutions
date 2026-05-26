d4c582fedaa89c4d36dbf96a720bfafa805a246b
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-05-26 03:32:58 +0000

    Sync LeetCode submission - Next Greater Element I (cpp)

--- problems/next_greater_element_i/solution.cpp
+++ problems/next_greater_element_i/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int, int> mpp;
         vector<int> ans;
-        for(int i = 0; i < nums1.size();i++){
-            int temp = nums1[i];
-            bool found = false;
-            for(int j = 0; j < nums2.size(); j++){
-                if(temp == nums2[j]) found = true;
-                if(found){
-                    if(nums2[j] > temp){
-                        ans.push_back(nums2[j]);
-                        break;
-                    }
-                    if(j == nums2.size() - 1) found = false;
-                }
-            }
-            if(!found) ans.push_back(-1);
+        for(int i = nums2.size()-1; i >= 0; i--){
+            while(!st.empty() && st.top()<= nums2[i]) st.pop();
+            if(!st.empty())mpp[nums2[i]] = st.top();
+            else mpp[nums2[i]] = -1;
+            st.push(nums2[i]);
+        }
+
+        ans.reserve(nums1.size());
+        for(int nums: nums1){
+            ans.push_back(mpp[nums]);
         }
 
         return ans;
