21a987a77153fa79a96a7f7a5cd32ac2790b3a14
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-06-16 04:21:34 +0000

    Sync LeetCode submission - Process String with Special Operations I (cpp)

---
+++ problems/process_string_with_special_operations_i/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result = "";
+        int n = s.size();
+        for(int i = 0; i < n; i++){
+            char ch = s[i];
+            if(ch >= 'a' && ch <= 'z') result.push_back(ch);
+            else if(ch == '*'){
+                if(!result.empty()) result.pop_back();
+            }
+            else if(ch == '#'){
+                result += result;
+            }
+            else if(ch == '%'){
+                reverse(result.begin(), result.end());
+            }
+        }
+        return result;
+    }
+};
