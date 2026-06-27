7f44e7c62954d225effa9bbbce5ece574c612d83
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-06-27 05:13:12 +0000

    Sync LeetCode submission - Maximum Sum Circular Subarray (cpp)

--- problems/maximum_sum_circular_subarray/solution.cpp
+++ problems/maximum_sum_circular_subarray/solution.cpp
@@ -4,15 +4,15 @@ public:
         int currMax = 0, currMin = 0;
         int maxSum = nums[0], minSum = nums[0];
         int n = nums.size();
-        int totalSum = 0;
+        int total = 0;
         for(int num : nums){
             currMax = max(currMax + num, num);
             maxSum = max(maxSum, currMax);
             currMin = min(currMin+num, num);
             minSum = min(minSum, currMin);
-            totalSum += num;
+            total += num;
         }
         if(maxSum < 0) return maxSum;
-        return max(maxSum, totalSum-minSum);
+        return max(maxSum, total-minSum);
     }
 };
